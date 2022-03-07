150d77ed6a1f58ce72aa1272ef14a9d0f48e3162
Kim Junha <kim_jh6859@naver.com>
2022-03-08 00:53:16 +0900

    modify js

--- lab1/main.js
+++ lab1/main.js
@@ -8,9 +8,13 @@ function makeObject(name, does, found, value) {
     return object;
 }
 
-const vitamins = [];
-const minerals = [];
+const vitamins = []; // vitamins object
+const minerals = [];  // minerals object
 
-vitamins.push(makeObject('Biotin', ['Energy storage', 'Protein, carbohygrate, and fat metabolism'], ['Avocados','Avocados','Cauliflower','Eggs','Fruits (e.g., raspberries)','Liver','Pork','Salmon','Whole grains'], '30 mcg'))
+vitamins.push(makeObject('Biotin', '• Energy storage\n• Protein, carbohydrate, and fat metabolism', '• Avocados\n• Cauliflower\n• Eggs\n• Fruits (e.g., raspberries)\n• Liver\n• Pork\n• Salmon\n• Whole grains', '30 mcg'))
+vitamins.push(makeObject('Choline','• Brain development \n• Cell signaling\n• Lipid (fat) transport and metabolism\n• Liver function\n• Muscle movement\n• Nerve function\n• Normal metabolism', '• Beans and peas\n• Egg yolks\n• Fish (e.g., cod and salmon)\n• Liver (e.g., beef and chicken)\n• Milk\n• Nuts\n• Salmon\n• Soy foods\n• Vegetables (e.g., broccoli, cauliflower, spinach)','550 mg'))
+vitamins.push(makeObject('Folate/Folic Acid', '• Prevention of birth defects\n• Protein metabolism \n• Red blood cell formation', '• Asparagus\n• Avocados\n• Beans and peas\n• Enriched grain products (e.g., bread, cereal, pasta, rice)\n• Green leafy vegetables (e.g., spinach)\n• Oranges and orange juice', '400 mcg DFE**'))
+vitamins.push(makeObject('Niacin'))
+
 
 alert(vitamins[0].name);
